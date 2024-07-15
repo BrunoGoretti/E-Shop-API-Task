7b15d52adccb7b6bba9f50bf8fc7f581a1512349
Gor <104521859+BrunoGoretti@users.noreply.github.com>
2024-07-15 16:41:36 +0400

    Add Models

---
+++ EShopAPI/Models/UserOrdersModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EShopAPI.Models
+{
+    public class Usermodel
+    {
+        [Key]
+        public int UserId { get; set; }
+        public int OrderNumber { get; set; }
+        public double PayableAmount { get; set; }
+        public string PaymentGateway { get; set; }
+        public string OptionalDescription { get; set; }
+    }
+}
