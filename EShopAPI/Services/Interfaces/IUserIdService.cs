85b6503782f23c07fd522004bd4489ac7a1a8b39
Gor <104521859+BrunoGoretti@users.noreply.github.com>
2024-07-15 18:53:31 +0400

    Deleye optional controllers

--- EShopAPI/Services/Interfaces/IUserIdService.cs
+++ EShopAPI/Services/Interfaces/IUserIdService.cs
@@ -4,6 +4,6 @@ namespace EShopAPI.Services.Interfaces
 {
     public interface IUserIdService
     {
-
+        Task<Usermodel> GetUserIdAsync(int userId);
     }
 }
--- EShopAPI/Services/UserIdService.cs
+++ EShopAPI/Services/UserIdService.cs
@@ -1,13 +1,26 @@
-﻿using EShopAPI.Services.Interfaces;
+﻿using EShopAPI.Data;
+using EShopAPI.Models;
+using EShopAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EShopAPI.Services
 {
     public class UserIdService : IUserIdService
     {
-        public IActionResult Index()
+        private readonly ApiContext _context;
+
+        public UserIdService(ApiContext context)
         {
-            Console.WriteLine("asd");
+            _context = context;
         }
+
+        public async Task<Usermodel> GetUserIdAsync(int userId)
+        {
+            var user = await _context.DbUsers.FirstOrDefaultAsync(u => u.UserId == userId);
+            return user;
+        }
+
+      
     }
 }
