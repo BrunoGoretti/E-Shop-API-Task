47f03e314c9c65b6a423460939464d05236b649a
Gor <104521859+BrunoGoretti@users.noreply.github.com>
2024-07-19 02:30:58 +0400

    Complete tests

---
+++ EShopAPI.nUnitTests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using NUnit.Framework;
+global using Moq;
+global using Microsoft.AspNetCore.Mvc;
+global using EShopAPI.Controllers;
+global using EShopAPI.Services.Interfaces;
+global using EShopAPI.Models;
+global using EShopAPI.Data;
+global using Microsoft.EntityFrameworkCore;
